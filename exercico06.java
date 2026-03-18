8276dfe77d4653ec132e9793436bc439879e4294
feitosapedrofatesg-svg <feitosapedrofatesg@gmail.com>
2026-03-17 21:50:08 -0300

    Add files via upload
    exercicio06

---
+++ exercico06.java
@@ -0,0 +1,18 @@
+package org.example.classes;
+
+public class exercico06 {
+
+    No cabeca;
+
+    public int contar() {
+
+        int contador = 0;
+        No atual = cabeca;
+
+        while (atual != null) {
+            contador++;
+            atual = atual.getProximo();
+        }
+        return contador;
+    }
+}
