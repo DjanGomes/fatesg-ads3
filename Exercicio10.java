f37b963ead79c8cc206876048dcfa7201f491b74
filipivg <filipivieiragiaconete@gmail.com>
2026-03-17 21:39:35 -0300

    feat(lista-encadeada): implementa método de remoção do último elemento
    - Adiciona método removerUltimo() na classe Exercicio3
    - Percorre a lista até o penúltimo nó e aponta seu próximo para null
    - Trata caso de lista vazia lançando RuntimeException
    - Trata caso de lista com único elemento definindo cabeca como null

---
+++ Exercicio10.java
@@ -0,0 +1,22 @@
+package Exercicio10;
+
+    public class Exercicio10 {
+    public No cabeca;
+    public void removerUltimo() {
+    if (cabeca == null) {
+        throw new RuntimeException("Lista vazia!");
+    }
+
+    if (cabeca.getProximo() == null) {
+        cabeca = null;
+        return;
+    }
+
+    No atual = cabeca;
+    while (atual.getProximo().getProximo() != null) {
+        atual = atual.getProximo();
+    }
+
+    atual.setProximo(null);
+}
+}
